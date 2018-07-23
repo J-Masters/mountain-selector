1c75e2a8a95207b0648e5bc8fd57febadccbccbe
justin <masterja29@gmail.com>
2018-07-22 22:07:42 -0400

    added keys, model, run script

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const keys = require('./config/keys');
+require('./models/mountain');
+
+mongoose.connect(keys.mongoURI, { useNewUrlParser: true });
+
 const app = express();
 
 app.get('/', (req, res) => {
