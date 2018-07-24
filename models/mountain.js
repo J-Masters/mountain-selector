341f53df868558d148548a5e148ff9b0174c1f77
justin <masterja29@gmail.com>
2018-07-24 13:42:59 -0400

    used create-react-app, set up front end file structure

--- models/mountain.js
+++ models/mountain.js
@@ -3,6 +3,20 @@ const { Schema } = mongoose;
 
 const mountainSchema = new Schema({
   name: String,
+  annualSnowfall: Number,
+  acreage: Number,
+  vertical: Number,
+  summitElevation: Number,
+  lifts: Number,
+  terrainParks: Number,
+  airportNearest: String,
+  driveTime: Number,
+  snowboardsAllowed: Boolean,
+  country: String,
+  stateOrProvince: String,
+  beginnerPercent: Number,
+  intermediatePercent: Number,
+  expertPercent: Number
 });
 
 mongoose.model('mountains', mountainSchema);
